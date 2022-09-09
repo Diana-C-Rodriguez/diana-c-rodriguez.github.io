6c564bc1ee4ebc721668912fd0c3e3554819650d
Diana Rodriguez <112515211+Diana-C-Rodriguez@users.noreply.github.com>
2022-09-09 13:30:06 -0500

    Update Fall.R

--- Fall.R
+++ Fall.R
@@ -66,7 +66,7 @@ for (action in actions)
 ggplot() +
   geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2),
                lineend = "round",
-               color="burlywood3", # Set your own Fall color?
+               color="purple3", # Set your own Fall color?
                data=na.omit(points)) +
   coord_fixed(ratio = 1) +
   theme_void() # No grid nor axes
